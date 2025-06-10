03fbf043fdbaed2ad3aa33408b864b15ad31f3fc
DanCru <=>
2025-06-10 15:48:38 +0700

    fix lại một số lỗi liên quan đến phần đa quốc gia

--- lavishstay-backend/app/Models/Language.php
+++ lavishstay-backend/app/Models/Language.php
@@ -2,12 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Language extends Model
-{
+{  
+    protected $table = 'language';
     protected $primaryKey = 'language_code';
     public $incrementing = false;
     protected $keyType = 'string';
     protected $fillable = ['language_code', 'name'];
+    public $timestamps = false;
 }
