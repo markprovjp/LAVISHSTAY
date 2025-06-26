fc6b9f8997dbcd73246d4f6b904ffa2312b77f8c
DanCru <=>
2025-06-27 01:04:36 +0700

    ..

--- lavishstay-backend/resources/views/admin/users/index.blade.php
+++ lavishstay-backend/resources/views/admin/users/index.blade.php
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-=======
 <style>
     .button-action {
         position: relative;
@@ -14,7 +12,6 @@
     }
 </style>
 
->>>>>>> origin/Phuoc
 <x-app-layout>
     <div class="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
         <!-- Page header -->
@@ -435,8 +432,4 @@
             }
         }
     </script>
-<<<<<<< HEAD
 </x-app-layout>
-=======
-</x-app-layout>
->>>>>>> origin/Phuoc
