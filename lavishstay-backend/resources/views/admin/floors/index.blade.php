de2080e61046d4d8bb066a43ea61ee11ca4cde1b
tuyenwebit <tuyenbocr7@gmail.com>
2025-08-16 11:57:41 +0700

    .

--- lavishstay-backend/resources/views/admin/floors/index.blade.php
+++ lavishstay-backend/resources/views/admin/floors/index.blade.php
@@ -15,7 +15,7 @@
                         <svg class="fill-current shrink-0 xs:hidden" width="16" height="16" viewBox="0 0 16 16">
                             <path d="M15 7H9V1c0-.6-.4-1-1-1S7 .4 7 1v6H1c-.6 0-1 .4-1 1s.4 1 1 1h6v6c0 .6.4 1 1 1s1-.4 1-1V9h6c.6 0 1-.4 1-1s-.4-1-1-1z"/>
                         </svg>
-                        <span class="max-xs:sr-only">Add Floor</span>
+                        <span class="max-xs:sr-only">Thêm Tầng</span>
                     </button>
                 </a>
             </div>
