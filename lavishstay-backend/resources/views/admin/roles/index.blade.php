e6c644d9139772625de2b998b7ae47fefbd60ed3
Lê Hiểu Phước <lehieuphuoc35205@gmail.com~>
2025-08-22 15:01:50 +0700

    bản sao

--- lavishstay-backend/resources/views/admin/roles/index.blade.php
+++ lavishstay-backend/resources/views/admin/roles/index.blade.php
@@ -11,7 +11,7 @@
         width: 200px;
     }
 </style>
-
+{{-- ban sao --}}
 <x-app-layout>
     <div class="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto"
          x-data="{ showAddModal: false, showEditModal: false, editRole: {}, editId: null }">
