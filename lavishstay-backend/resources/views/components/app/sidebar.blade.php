174bd99c82ea1fd92c8659a908d8ac81db538c1d
DanCru <nguyenanhduc2909@gmail.com>
2025-08-27 23:24:32 +0700

    Merge code với Tuyên

--- lavishstay-backend/resources/views/components/app/sidebar.blade.php
+++ lavishstay-backend/resources/views/components/app/sidebar.blade.php
@@ -692,6 +692,16 @@
                                             <span class="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">Phụ thu trẻ em</span>
                                         </a>
                                     </li>
+                                    <li class="mb-1 last:mb-0">
+                                        @if (Auth::user()?->hasPermission('chinh_sach'))
+                                            <a class="block text-gray-500/90 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200 transition-colors duration-200 truncate @if (request()->routeIs('admin.compensation-policies*')) {{ 'text-violet-500!' }} @endif"
+                                                href="{{ route('admin.compensation-policies') }}">
+                                                <span
+                                                    class="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">Chính
+                                                    sách bồi thường</span>
+                                            </a>
+                                        @endif
+                                    </li>
                                 </ul>
                             </div>
                         </li>
